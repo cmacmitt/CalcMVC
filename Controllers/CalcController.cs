79e1d4f8bfe48b79b6622a7cabe9c22e10fbce3d
Chris MacDonald <chris.macdonald@mitt.ca>
2022-11-29 10:52:06 -0600

    Add project files.

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalcMVC.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            ViewBag.Result = result;
+            return View();
+        }
+
+        public IActionResult Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            ViewBag.Result = result;
+            return View();
+        }
+
+        public IActionResult Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            ViewBag.Result = result;
+            return View();
+        }
+
+        public IActionResult Divide(int num1, int num2)
+        {
+            if (num2 != 0)
+            {
+                ViewBag.Result = num1 / num2;
+            }
+            else
+            {
+                return Content("<h2>You cannot divide by zero</h2>", "text/html");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalcMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
